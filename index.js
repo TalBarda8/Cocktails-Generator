202606411abe3d5d46ddcc62316099bda9faa425
Tal Barda <talbarda8@gmail.com>
2023-11-06 17:49:59 +0200

    set api integration

--- index.js
+++ index.js
@@ -4,10 +4,48 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+function getIngredients(drink) {
+    const ingredients = [];
+    const measures = [];
+
+    for (let i = 1; i <= 15; i++) {
+        const ingredientKey = `strIngredient${i}`;
+        const measureKey = `strMeasure${i}`;
+        const ingredient = drink[ingredientKey];
+        const measure = drink[measureKey];
+
+        if (ingredient && measure) {
+            ingredients.push(ingredient);
+            measures.push(measure);
+        }
+    }
+
+    const combined = ingredients.map((ing, index) => {
+        return { ingredient: ing, measure: measures[index] };
+    });
+
+    return combined;
+}
+
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+        const drink = result.data.drinks[0];
+        const ingredients = getIngredients(drink);
+
+        res.render("index.ejs", {
+            name: drink.strDrink,
+            picture: drink.strDrinkThumb,
+            glass: drink.strGlass,
+            instructions: drink.strInstructions,
+            ingredients: ingredients,
+        })
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
-})
+});
